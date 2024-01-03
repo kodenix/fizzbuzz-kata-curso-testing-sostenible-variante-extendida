b244c2b091c7497481cb5eaa4b98afd1c5c8f5fd
Alberto Morales <kodenix@gmail.com>
2024-01-03 19:00:07 +0100

    Refactorized test

--- src/tests/fizzbuzz.test.ts
+++ src/tests/fizzbuzz.test.ts
@@ -1,15 +1,8 @@
 import { FizzBuzz } from '../core/fizzBuzz';
 
 describe('FizzBuzz', ()=> {
-	test('convert_1_to_1', ()=>{
-		expect(new FizzBuzz().convert(1)).toBe('1');
-	})
 
-	test('convert_2_to_2', ()=>{
-		expect(new FizzBuzz().convert(2)).toBe('2');
-	})
-
-	test('convert_4_to_4', ()=>{
-		expect(new FizzBuzz().convert(4)).toBe('4');
+	test.each([[1, "1"], [2, "2"], [4, "4"]])('convert %i to character representation', (value: number, expected: string)=>{
+		expect(new FizzBuzz().convert(value)).toBe(expected);
 	})
 })
