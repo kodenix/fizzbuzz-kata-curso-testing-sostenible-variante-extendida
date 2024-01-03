75c9b909e24f142f69bf085f4df4f8954639528e
Alberto Morales <kodenix@gmail.com>
2024-01-03 19:05:40 +0100

    Added first test for Fizz rule

--- src/core/fizzBuzz.ts
+++ src/core/fizzBuzz.ts
@@ -1,5 +1,6 @@
 export class FizzBuzz {
     convert(value: number): string {
+        if (value === 3) return "Fizz";
         return value.toString();
     }
 }
--- src/tests/fizzbuzz.test.ts
+++ src/tests/fizzbuzz.test.ts
@@ -5,4 +5,8 @@ describe('FizzBuzz', ()=> {
 	test.each([[1, "1"], [2, "2"], [4, "4"]])('convert %i to character representation', (value: number, expected: string)=>{
 		expect(new FizzBuzz().convert(value)).toBe(expected);
 	})
+
+	test('convert_3_to_Fizz', () => {
+		expect(new FizzBuzz().convert(3)).toBe("Fizz");
+	});
 })
