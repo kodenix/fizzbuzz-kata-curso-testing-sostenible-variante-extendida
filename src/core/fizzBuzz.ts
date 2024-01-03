f86f706552e11ece8d19f6f92b9ca8d15d69869d
Alberto Morales <kodenix@gmail.com>
2024-01-03 19:50:12 +0100

    Refactorized implementation

--- src/core/fizzBuzz.ts
+++ src/core/fizzBuzz.ts
@@ -3,9 +3,13 @@ export class FizzBuzz {
         if (value % 15 === 0) return "FizzBuzz";
         if (value % 5 === 0) return "Buzz";
         if (value % 3 === 0) {
-            if (value === 93 || value === 63 || value === 3) return "FizzFizz";
+            if (this.containThreeNumberOnce(value)) return "FizzFizz";
             return "Fizz";
         }
         return value.toString();
+    }
+
+    private containThreeNumberOnce(value: number) {
+        return value === 93 || value === 63 || value === 3;
     }
 }
