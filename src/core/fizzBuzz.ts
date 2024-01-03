e8772b6bd86f0a6dea29572c9133e953a8ba23aa
Alberto Morales <kodenix@gmail.com>
2024-01-03 18:55:51 +0100

    Refactorized implementation

--- src/core/fizzBuzz.ts
+++ src/core/fizzBuzz.ts
@@ -1,7 +1,5 @@
 export class FizzBuzz {
     convert(value: number): string {
-        if (value === 2) return "2";
-        if (value === 4) return "4";
-        return "1";
+        return value.toString();
     }
 }
