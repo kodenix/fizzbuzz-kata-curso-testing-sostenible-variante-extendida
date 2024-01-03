a2c76f68db4cb3b74114b5cfc0eb2a3eabe8cd81
Alberto Morales <kodenix@gmail.com>
2024-01-03 23:34:05 +0100

    added test values

--- src/tests/fizzbuzz.test.ts
+++ src/tests/fizzbuzz.test.ts
@@ -2,34 +2,22 @@ import { FizzBuzz } from '../core/fizzBuzz';
 
 describe('FizzBuzz', ()=> {
 
-	test.each([[1, "1"], [2, "2"], [4, "4"]])('convert %i to character representation', (value: number, expected: string)=>{
-		expect(new FizzBuzz().convert(value)).toBe(expected);
-	})
-
-	test.each([[6, "Fizz"], [9, "Fizz"]])('convert %i to %s', (value: number, expected: string)=>{
-		expect(new FizzBuzz().convert(value)).toBe(expected);
-	})
-
 	test.each([
+		[1, "1"], 
+		[2, "2"], 
+		[4, "4"],
+		[1543, "1543"],
+		[6, "Fizz"], 
+		[9, "Fizz"],
 		[5, "BuzzBuzz"],
 		[550, "BuzzBuzzBuzz"], 
 		[10, "Buzz"], 
-		[20, "Buzz"]
-	])('convert %i to %s', (value: number, expected: string)=>{
-		expect(new FizzBuzz().convert(value)).toBe(expected);
-	})
-
-	test.each([
+		[20, "Buzz"],
 		[15, "FizzBuzzBuzz"], 
 		[555, "FizzBuzzBuzzBuzzBuzz"], 
 		[30, "FizzBuzzFizz"], 
 		[60, "FizzBuzz"],
 		[3000, "FizzBuzzFizz"],
-	])('convert %i to %s', (value: number, expected: string)=>{
-		expect(new FizzBuzz().convert(value)).toBe(expected);
-	})
-
-	test.each([
 		[3, "FizzFizz"], 
 		[33, "FizzFizzFizz"], 
 		[63, "FizzFizz"], 
@@ -38,6 +26,5 @@ describe('FizzBuzz', ()=> {
 	])('convert %i to %s', (value: number, expected: string)=>{
 		expect(new FizzBuzz().convert(value)).toBe(expected);
 	})
-
 	
 })
