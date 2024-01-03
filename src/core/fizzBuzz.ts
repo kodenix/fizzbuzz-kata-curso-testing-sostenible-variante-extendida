61d200dbb1ed90963dee5c1a81cb0fb553de135d
Alberto Morales <kodenix@gmail.com>
2024-01-03 19:45:48 +0100

    Refactorized implementation

--- src/core/fizzBuzz.ts
+++ src/core/fizzBuzz.ts
@@ -1,14 +1,13 @@
 export class FizzBuzz {
     convert(value: number): string {
+        if (value % 15 === 0) return "FizzBuzz";
+        if (value % 5 === 0) return "Buzz";
         if (value % 3 === 0) {
             if (value === 93) return "FizzFizz"
             if (value === 63) return "FizzFizz"
             if (value === 3) return "FizzFizz"
+            return "Fizz";
         }
-        
-        if (value % 15 === 0) return "FizzBuzz";
-        if (value % 5 === 0) return "Buzz";
-        if (value % 3 === 0) return "Fizz";
         return value.toString();
     }
 }
