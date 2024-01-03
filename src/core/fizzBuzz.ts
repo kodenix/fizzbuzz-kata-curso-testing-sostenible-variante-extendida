ef23228bab2780102ee7a681823161485fdace60
Alberto Morales <kodenix@gmail.com>
2024-01-03 23:44:55 +0100

    refactorized

--- src/core/fizzBuzz.ts
+++ src/core/fizzBuzz.ts
@@ -1,39 +1,48 @@
 export class FizzBuzz {
+    private readonly BUZZ_TEXT = "Buzz";
+    private readonly FIZZ_TEXT = "Fizz";
+    private readonly THREE_VALUE = 3;
+    private readonly FIVE_VALUE = 5;
+    
     convert(value: number): string {
-        if (value % 15 === 0) {
+        if (this.isDivisibleByNumber(value, this.THREE_VALUE * this.FIVE_VALUE)) {
             return this.processDivisibleByFiveteen(value);
         }
-        if (value % 5 === 0) {
-            return "Buzz"+this.processDivisibleByFive(value);
+        if (this.isDivisibleByNumber(value, this.FIVE_VALUE)) {
+            return this.BUZZ_TEXT+this.processDivisibleByFive(value);
         }
-        if (value % 3 === 0) {
-            return "Fizz"+this.processDivisibleByThree(value);
+        if (this.isDivisibleByNumber(value, this.THREE_VALUE)) {
+            return this.FIZZ_TEXT+this.processDivisibleByThree(value);
         }
         return value.toString();
     }
     
+    private isDivisibleByNumber(value: number, dividend: number): boolean {
+        return value % dividend === 0;
+    }
+
     private processDivisibleByFiveteen(value: number): string {
-        let outText = "FizzBuzz";
+        let outText = this.FIZZ_TEXT+this.BUZZ_TEXT;
         outText+=this.processDivisibleByFive(value);
         outText+=this.processDivisibleByThree(value);
         return outText;
     }
     
     private processDivisibleByFive(value: number): string {
-        const totalCharacters = this.totalOfCharacterInValue(value, "5");
+        const totalCharacters = this.totalOfCharacterInValue(value, this.FIVE_VALUE.toString());
         let outText = "";
         for (let i=0; i < totalCharacters; i++) {
-            outText+= "Buzz";
+            outText+= this.BUZZ_TEXT;
         }
 
         return outText;
     }
     
     private processDivisibleByThree(value): string {
-        const totalCharacters = this.totalOfCharacterInValue(value, "3");
+        const totalCharacters = this.totalOfCharacterInValue(value, this.THREE_VALUE.toString());
         let outText = "";
         for (let i=0; i < totalCharacters; i++) {
-            outText+= "Fizz";
+            outText+= this.FIZZ_TEXT;
         }
 
         return outText;
