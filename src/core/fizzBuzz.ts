0b4c644aa0e858b3c22a3725a9053471e9f8ec76
Alberto Morales <kodenix@gmail.com>
2024-01-03 22:53:04 +0100

    refactorized

--- src/core/fizzBuzz.ts
+++ src/core/fizzBuzz.ts
@@ -3,14 +3,18 @@ export class FizzBuzz {
         if (value % 15 === 0) return "FizzBuzz";
         if (value % 5 === 0) return "Buzz";
         if (value % 3 === 0) {
-            if (this.totalOfThreeNumber(value, "3")===2) return "FizzFizzFizz";
-            if (this.totalOfThreeNumber(value, "3")===1) return "FizzFizz";
-            return "Fizz";
+            return this.processDivisibleByThree(value);
         }
         return value.toString();
     }
+    
+    private processDivisibleByThree(value): string {
+        if (this.totalOfCharacterInValue(value, "3")===2) return "FizzFizzFizz";
+        if (this.totalOfCharacterInValue(value, "3")===1) return "FizzFizz";
+        return "Fizz";
+    }
 
-    private totalOfThreeNumber(value: number, character: string = "3"): number {
+    private totalOfCharacterInValue(value: number, character: string): number {
         const stringfiedValue = value.toString()
         let countThreeNumber = 0;
         for (let i = 0; i < stringfiedValue.length; i ++) {
